e156d0b7ce8313b18923e9863be6f973ed5c6021
Beesama <gbozee@gmail.com>
2020-02-01 05:14:18 +0100

    made orm dependency conditional

--- sstarlette/__init__.py
+++ sstarlette/__init__.py
@@ -1,5 +1,5 @@
 from sstarlette.base import SStarlette, SResult
 # from starlette import authentication
 
-__version__ = "0.0.12"
+__version__ = "0.0.17"
 # __all__ = ["SStarlette", "SResult", "authentication"]
